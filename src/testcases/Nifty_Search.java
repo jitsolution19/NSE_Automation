94a5051d6f93544728eb39884580d27a5b5f7a5f
jitsolution19 <jitsolution19@gmail.com>
2017-12-16 02:30:32 +0530

    test

--- src/testcases/Nifty_Search.java
+++ src/testcases/Nifty_Search.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Nifty_Search {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		System.setProperty("webdriver.chrome.driver", "B:\\Automation\\software\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.nseindia.com/");
-		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		Thread.sleep(5000);
+//		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
 		System.out.println("Normal Market Status :: "+driver.findElement(By.xpath("//span[@id='status1']")).getText().trim());
 		System.out.println("Nifty 50 Today's Price :: "+driver.findElement(By.xpath("//p[@class='right']//nobr")).getText().trim());
 		System.out.println("Nifty 50 Today's Price Change :: "+driver.findElement(By.xpath("//nobr/span[1]")).getText().trim());
--- src/testcases/temp.java
+++ src/testcases/temp.java
@@ -1,10 +1,25 @@
 package testcases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class temp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+	System.out.println("Hello");
+	System.setProperty("webdriver.ie.driver", "B:\\Automation\\software\\IEDriverServer\\IEDriverServer.exe");
+	WebDriver driver = new InternetExplorerDriver();
+	driver.navigate().to("https://www.nseindia.com/");
+	driver.manage().timeouts().pageLoadTimeout(50,TimeUnit.SECONDS);
+	driver.findElement(By.id("keyword")).sendKeys("HDFC");
+	driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+//	System.out.println("Normal Market Status :: "+driver.findElement(By.xpath("//span[@id='status1']")).getText().trim());
+
 	}
 
 }
