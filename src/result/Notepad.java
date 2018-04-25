78d93edcaac9496cb297fb7279de1330ae80f0dc
jitsolution19 <jitsolution19@gmail.com>
2018-04-25 09:33:49 +0530

    changes made to reporting

--- src/result/Notepad.java
+++ src/result/Notepad.java
@@ -3,7 +3,6 @@ package result;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,32 +10,15 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Notepad {
+	static String FolderPath="C:\\Users\\mamta\\eclipse-workspace\\NSE_INDIA\\Result";
 	public static void main(String[] args){
-	String filePath= "C:\\Users\\mamta\\eclipse-workspace\\NSE_INDIA\\Result\\TestResult";
+	String filePath= FolderPath+"\\input.txt";
 	String value=readtextfile(filePath);
 	System.out.println(value);
-//		
-//	BufferedWriter writer = null;
-//    try {
-//        //create a temporary file
-//        String timeLog = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-//        File file = new File("C:\\Users\\mamta\\eclipse-workspace\\NSE_INDIA\\Result\\TestResult");
-//        File logFile = new File(timeLog);
-//
-//        // This will output the full path where the file will be written to...
-//        System.out.println(logFile.getCanonicalPath());
-//
-//        writer = new BufferedWriter(new FileWriter(logFile));
-//        writer.write("Hello world!");
-//    } catch (Exception e) {
-//        e.printStackTrace();
-//    } finally {
-//        try {
-//            // Close the writer regardless of what happens...
-//            writer.close();
-//        } catch (Exception e) {
-//        }
-//    }
+	
+	String testCaseName="TC01_Test";
+	String text ="Hello World";
+	writetextfile(FolderPath,testCaseName,text);
 }
 	public static String readtextfile(String filePath)
 	{
@@ -52,5 +34,26 @@ public class Notepad {
 		}
 		return data;	 
 			
+	}
+	
+	public static void writetextfile(String FolderPath,String testCaseName,String text)
+	{
+		BufferedWriter writer = null;		
+		String timeLog = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime());	
+		File resultfile = new File(FolderPath+"\\"+testCaseName+"_"+timeLog+".txt");
+		try {
+			writer = new BufferedWriter(new FileWriter(resultfile));
+			writer.write(text);
+		} catch (IOException e) {	
+			e.printStackTrace();
+		}finally {
+	      try {
+//	       Close the writer regardless of what happens...
+	      writer.close();
+	  } catch (Exception e)
+	     {
+		  e.printStackTrace();
+	     }
+	  }	
 	}
 }
