d15bd3342d62b4457a7fa2d550d0eb11f31d910c
jitsolution19 <jitsolution19@gmail.com>
2018-04-25 10:17:53 +0530

    updated

--- Environment_Setup/database/Testing.java
+++ Environment_Setup/database/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		String currentDirectory=System.getProperty("user.dir");
-		String filePath= currentDirectory+"\\Result\\input.txt";		
+		String filePath= currentDirectory+"\\InputFiles\\input.txt";		
 		String value=database.ReadFromTextFile.readtextfile(filePath);
 		System.out.println(value);
 		
