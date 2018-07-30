ee5ac59ef5e3f64bb3e8c0d437e165a0242309eb
jitsolution19 <jitsolution19@gmail.com>
2018-07-30 23:41:13 +0530

    update

--- src/testcases/TopGainer_And_Looser.java
+++ src/testcases/TopGainer_And_Looser.java
@@ -28,22 +28,27 @@ public class TopGainer_And_Looser {
 		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
 		int retu= Stockinfo(driver,"Gainers","Nifty 50");
 		System.out.println(retu);
-		
+		System.out.println("List of Looser are as ::");
+		int looserinfo= Stockinfo(driver,"looser","Nifty 50");
+		System.out.println(looserinfo);
 	}
 
 	public static int Stockinfo(WebDriver driver,String Gainers_Looser,String view_active )
 	{
+		System.out.println("Inside Stock info Page");
+		System.out.println(driver.findElement(By.id("dataTime")).getText());
 		String TableMathod;
 		if(Gainers_Looser.matches("Gainers"))
 		{
+			driver.findElement(By.xpath("//a[contains(text(),'Gainers') and @id='tab7']")).click();
 			TableMathod = "topGainers";	
+			driver.findElement(By.id("Gnifty")).click();
 		}else
 		{
+			driver.findElement(By.xpath("//a[contains(text(),'Losers') and @id='tab8']")).click();
 			TableMathod = "topLosers";
+			driver.findElement(By.id("Lnifty")).click();
 		}
-		driver.findElement(By.linkText(Gainers_Looser)).click();
-		System.out.println(driver.findElement(By.id("dataTime")).getText());
-		driver.findElement(By.linkText(view_active)).click();
 		List<WebElement> TabelRow= driver.findElements(By.xpath("//table[@id='"+TableMathod+"']/tbody/tr"));
 		System.out.println(TabelRow.size());
 		System.out.print("Symbol|LTP|% Change|Traded Qty|Value(in Lakhs)|Open|High|Low|Prev. Close|Latest Ex Date|Corporate Action|");
