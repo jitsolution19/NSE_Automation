4a0b5459191351aab1c2600ef53beaeb5b9c01b7
jitsolution19 <jitsolution19@gmail.com>
2018-09-08 19:57:59 +0530

    update

--- src/result/htmlResult.java
+++ src/result/htmlResult.java
@@ -33,7 +33,7 @@ public class htmlResult {
 			junitWriter = new BufferedWriter(new FileWriter(junitReport, true));
 			junitWriter.write("<!DOCTYPE html><html>");
 			junitWriter.write("<h1 align=\"center\" style=\"background-color:DodgerBlue;\">Test Execution Summary - " + dateFormat.format(date)+ "</h1>");	
-			junitWriter.write("<h2 style=\"background-color:DodgerBlue;\"><span style=\"float:left;\">Test case Name :: Testing </span><span style=\"float:right;\">Executed By :: Jitendra</span></h2>");
+			junitWriter.write("<h2 style=\"background-color:DodgerBlue;\"><span style=\"float:left;\">Scenario Name :: Testing </span><span style=\"float:right;\">Executed By :: Jitendra</span></h2>");
 			junitWriter.write("<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 15px;}</style></head>");
 			junitWriter.write("<body>");
 			
@@ -41,34 +41,34 @@ public class htmlResult {
 			junitWriter.write("<th>S.no#</th><th>Test Case Name</th><th>Expected Result</th><th>Actual Result</th><th>Status</th><th>Start Time</th><th>End Time</th>");
 			
 			
-			junitWriter.write("<tr>");
-			junitWriter.write("<td align=\"center\">1</td>");
-			junitWriter.write("<td>Verify User Name</td>");
-			junitWriter.write("<td>jitsolution19@gmail.com</td>");
-			junitWriter.write("<td>jitsolution19@gmail.com</td>");
-			junitWriter.write("<td align=\"center\"><font color=\"Green\">Pass</font></td>");
-			SimpleDateFormat Starttime = new SimpleDateFormat("HH:mm:ss");
-
-		    Date now = new Date();
-
-		    String strTime = Starttime.format(now);
-
-//		    System.out.println("Time: " + strTime);
-			junitWriter.write("<td align='center'>"+strTime+"</td>");
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			SimpleDateFormat EndTime = new SimpleDateFormat("HH:mm:ss");
-			Date now1 = new Date();
-
-		    String EndTim = EndTime.format(now1);
-
-			junitWriter.write("<td align=\"center\">"+EndTim+"</td>");
-			junitWriter.write("</tr>");
-			
+//			junitWriter.write("<tr>");
+//			junitWriter.write("<td align=\"center\">1</td>");
+//			junitWriter.write("<td>Verify User Name</td>");
+//			junitWriter.write("<td>jitsolution19@gmail.com</td>");
+//			junitWriter.write("<td>jitsolution19@gmail.com</td>");
+//			junitWriter.write("<td align=\"center\"><font color=\"Green\">Pass</font></td>");
+//			SimpleDateFormat Starttime = new SimpleDateFormat("HH:mm:ss");
+//
+//		    Date now = new Date();
+//
+//		    String strTime = Starttime.format(now);
+//
+////		    System.out.println("Time: " + strTime);
+//			junitWriter.write("<td align='center'>"+strTime+"</td>");
+//			try {
+//				Thread.sleep(5000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			SimpleDateFormat EndTime = new SimpleDateFormat("HH:mm:ss");
+//			Date now1 = new Date();
+//
+//		    String EndTim = EndTime.format(now1);
+//
+//			junitWriter.write("<td align=\"center\">"+EndTim+"</td>");
+//			junitWriter.write("</tr>");
+//			
 			for(int i =1;i<=10;i++)
 			{
 				result(i);
